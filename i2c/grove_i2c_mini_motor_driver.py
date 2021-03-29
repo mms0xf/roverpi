e36906037ceb603b265c2c92d8ce15d310378278
mms <mms@mmsnoMacBook.local>
2021-03-29 12:08:44 +0900

    add driver features

--- i2c/grove_i2c_mini_motor_driver.py
+++ i2c/grove_i2c_mini_motor_driver.py
@@ -6,7 +6,7 @@
 
 import smbus
 import time
-i2c = smbus.SMBus(1)  #1 : bus number
+import enum
 
  
 #define FAULT 0x01 //レジスタのサブドレス
@@ -31,6 +31,9 @@ min - max
 111111 01 ←正転5V
 100101 10 ←逆転3V
 000000 11 ←ブレーキ
+
+000110(06) minSpeed
+111111(3F) maxSpeed
 '''
 
 '''
@@ -40,10 +43,12 @@ RaspberryPiでもシフト必要
 by i2sdetect-y 1の結果より
 '''
 
-rightWrite= 0x65 # 0xca
-rightRead= 0x66 # 0xcb
+#ch1
+channel1_write= 0x65 # 0xca
+channel1_read= 0x66 # 0xcb
 
-leftWrite = 0x60 # 0xC0
+#ch2
+channel2_write = 0x60 # 0xC0
 leftRead = 0x61 # 0xC1
 
 _command=0x00
@@ -51,44 +56,107 @@ _command=0x00
 
 _accel = 0b11111101 # 5v
 
-i2c.write_byte_data(leftWrite, _command, _accel)
-i2c.write_byte_data(rightWrite, _command, _accel)
-time.sleep(10)
-i2c.write_byte_data(leftWrite, _command, 0)
-i2c.write_byte_data(rightWrite, _command, 0)
+_minSpeed = 0b000110 #(06)
+_maxSpeed = 0b111111 #(3F)
+
+_fore = 0b01
+_back = 0b10
+
+# get direction bit (fore/back)
+def _direction_bit(is_fore):
+    # fore : 01
+    # back : 10
+    if is_fore is True:
+        return _fore
+    else:
+        return _back
+
+# get speed bit for vset from 0.0-1.0 range
+def _speed_bit(speed_rate):# 0-1
+    if speed_rate < 0:
+        speed_rate = 0
+        
+    if speed_rate > 1:
+        speed_rate = 1
+
+    length = _maxSpeed - _minSpeed
+    
+    speed_bit = int(length * speed_rate) + _minSpeed
+
+    
+    return speed_bit
+
+def _merge_speed_and_direction(speed_bit, dir_bit):
+    return (speed_bit << 2) + dir_bit
+
+# get writing channel address from 1 or 2
+def _writing_channel_bit(channel_no):
+    if channel_no = 1:
+        return channel1_write
+    elif channel_no = 2:
+        return channel2_write
+    else:
+        raise ValueError("unsupperted channel no")
+        
+def run(channel_no, speed_rate, is_fore):
+
+    speed = _speed_bit(speed_rate)
+    dir = _direction_bit(is_fore)
+
+    accel_bit = _merge_speed_and_direction(speed, dir):
+
+    channel_bit = _writing_channel_bit(channel_no)
+    i2c.write_byte_data(channel_bit, _command, accel_bit)
 
 
-#define FAULT 0x01  // 00000001
-#define CLEAR 0x80  // 10000000
-#define ILIMIT 0x10 // 00010000 電流制限
-#define OTS 0x08    // 00001000 過熱
-#define UVLO 0x04   // 00000100 低電圧誤動作防止
-#define OCP 0x02    // 00000010　過電流
+
+
+def stop():
+    i2c.write_byte_data(channel2_write, _command, 0)
+    i2c.write_byte_data(channel1_write, _command, 0)
+
+
 
 _clear = 0b10000000
 fallt_mask = 0b00000001
-limit_mask = 0b00010000
-ots_mask = 0b00001000
-uvlo_mask = 0b00000100
-ocp_mask = 0b00000010
-'''
-while 1:
-    res = read_byte_data(rightRead, _command)
+limit_mask = 0b00010000 # 電流制限
+ots_mask = 0b00001000 # 過熱
+uvlo_mask = 0b00000100 # 低電圧誤動作防止
+ocp_mask = 0b00000010 # 過電流
+
+
+# reset
+def reset_error(channel_no):
+    channel_address = _writing_channel_bit(channel_no)
+    i2c.write_byte_data(channel_address, _command, _clear)
+
+# enum
+class Fault(enum.Enum):
+    CURRENT_LIMITATION=auto()
+    OVER_TEMPTURE=auto()
+    PREVENT_LOW_VOLTAGE_OPERATION=auto()
+    OVER_CURRENT=auto()
+
+
+# get status. use on the endless loop.
+def get_status(channel_no):
+    channel_address = _writing_channel_bit(channel_no)
+    res = read_byte_data(channel_address, _command)
     if (res&fault_mask):
         if (res&limit_mask):
-            print('current limitation')
+            return Fault.CURRENT_LIMITATION
         if (res&ots_mask):
-            print('over tempture state')
-        if (res&ots_mask):
-            print('over tempture state')
+            return Fault.OVER_TEMPTURE
         if (res&uvlo_mask):
-            print('over tempture state')
+            return Fault.PREVENT_LOW_VOLTAGE_OPERATION
         if (res&ocp_mask):
-            print('over current')
-        # clear error status
-        i2c.write_byte_data(rightWrite, _command, _clear)
-    
-    
-    
-    time.sleep(1)
-'''
+            return Fault.OVER_CURRENT
+
+
+if __name__ == "__main__":
+
+    i2c = smbus.SMBus(1)  #1 : bus number, fixed on raspberry pi
+    run(channel_no=1, speed_rate=1, is_fore=true)
+    run(channel_no=2, speed_rate=1, is_fore=true)
+    time.sleep(10)
+    stop()
